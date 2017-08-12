8f95a15050339e434c65725facdab08e53ec58cc
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-12 23:33:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1', function (req,res){
+app.get('/article1', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article2', function (req,res){
+app.get('/article2', function (req, res){
    res.send("Article two has been requested ");
 });
 
-app.get('/article3', function (req,res){
+app.get('/article3', function (req, res){
    res.send("Article three has been requested ");
 });
 
