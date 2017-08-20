ad6343c0449b98516e243a900774891f4adc41c3
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-20 18:50:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content{
+    title = 'Article-1 | About';
+    heading = 'Article-1';
+    date = 'August 11,2017';
+}
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
