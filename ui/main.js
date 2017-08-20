f2687397a73df6535828dcf391a139d92b079f76
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-20 22:30:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById("main");
 element.innerHTML = 'new value';
 
 //move the image
-var img = document.getElementById("img");
-img.onclick = function () {
-    img.style.marginleft = "100px";
+var img1 = document.getElementById("img");
+img1.onclick = function () {
+    img1.style.marginLeft = "100px";
 };
