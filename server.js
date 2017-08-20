2f971176af16f042c1091b41c4d5528b0a5c0c16
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-20 19:09:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,45 +19,53 @@ var article1 = {
             </p>`
 };
 
-var htmltemplate =
-`
-<html>
-<head>
-<title>${title}</title>
-<meta name="viewport" content="width-device-width,initial-scale=1" />
-<link href="/ui/style.css" rel="stylesheet" />
-</head>
-
-<body>
-<div class="container">
-
-
-<div>
-<a href="/">Home Page</a><br/>
-<a href="/article2">Article-2</a>
-</div>
-<hr/>
-<h3>${heading}</h3>
-
-<div>
-${date}
-</div>
-<hr/>
-
-<div>
-<img src="ui/rameswaram.jpg" width="100%" height="50%">
-</div>
-
-<div>
-${content}
-</div>
-
-
-</div>
-</body>
-</html>
-`;
-
+function createtemplate(data){
+    
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    
+    var htmltemplate =
+    `
+    <html>
+    <head>
+    <title>${title}</title>
+    <meta name="viewport" content="width-device-width,initial-scale=1" />
+    <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    <div class="container">
+    
+    
+    <div>
+    <a href="/">Home Page</a><br/>
+    <a href="/article2">Article-2</a>
+    </div>
+    <hr/>
+    <h3>${heading}</h3>
+    
+    <div>
+    ${date}
+    </div>
+    <hr/>
+    
+    <div>
+    <img src="ui/rameswaram.jpg" width="100%" height="50%">
+    </div>
+    
+    <div>
+    ${content}
+    </div>
+    
+    
+    </div>
+    </body>
+    </html>
+    `;
+}
 
 
 
