0ac5374a2d62a1ee0afb05f63752bce5b4e1d081
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-25 11:08:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,23 +76,7 @@ function createtemplate(data){
     <html>
     <head>
     <title>${title}</title>
-    <link rel="apple-touch-icon" sizes="57x57" href="/ui/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/ui/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/ui/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/ui/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/ui/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/ui/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/ui/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/ui/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/ui/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="/ui/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/ui/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/ui/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/ui/favicon-16x16.png">
-    <link rel="manifest" href="/ui/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ui/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="icon" href="/ui/favicon-16x16.png"/>
     <meta name="viewport" content="width-device-width,initial-scale=1" />
     <link href="/ui/style.css" rel="stylesheet" />
     </head>
