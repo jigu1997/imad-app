45b75799531d4ca82963b5b2f85398cff664b1a6
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-26 16:27:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,20 +6,15 @@ var element = document.getElementById("maincontent");
 element.innerHTML = 'new value';
 
 //move the image
+
 var margin = 0;
+
 var img = document.getElementById("madiimg");
 function moveright(){
   margin = margin + 10;
   img.style.marginLeft = margin + "px";
 }
 
-var margin1 = 0;
-function moveleft(){
-  margin1 = margin1 + 10;
-  img.style.marginRight = margin1 + "px";
-}
 img.onclick = function(){
     var interval = setInterval(moveright,100);
-    sleep(100);
-    var inte = setInterval(moveleft,100);
 };
