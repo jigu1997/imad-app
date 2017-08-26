a5500835f6399b599c265c1b4170fdad94d4754e
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-26 17:59:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,26 @@
 
 var button = document.getElementById("counterbutton");
 
-var counter = 0;
-
 button.onclick = function(){
-    //Render counter in span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    
+    //Make Request
+    var request = new XMLHttpRequest();
+    
+    //Capture Response and Render
+    request.onreadystatechange = function(){
+        
+        if(request.readyState == XMLHttpRequest.DONE){
+            
+            if(request.status == 200){
+                var counter = request.responseText;
+                counter = counter + 1;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+                
+            }
+            
+        }
+        
+    };
+    
 };
