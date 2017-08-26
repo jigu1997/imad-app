b7d9ac904172efd7425e60b6cae2010b5b07a391
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-26 21:31:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -45,21 +45,22 @@ submit.onclick = function(){
          
          if(request.status == 200){
              
-             
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for(var i=0;i<names.length;i++){
+            list = list +"<li>"+names[i]+"</li>";
+            }
+            var ul = document.getElementById("namelist");
+            ul.innerHTML = list;
+
              
          }
          
      } 
         
     };
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0;i<names.length;i++){
-        list = list +"<li>"+names[i]+"</li>";
-    }
-    var ul = document.getElementById("namelist");
-    ul.innerHTML = list;
-    
+  
     request.open("GET","http://jigu1997.imad.hasura-app.io/submit-name?="+name,true);
     request.send(null);
     
