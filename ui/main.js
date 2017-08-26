8255fdbeb6e6149ed08acf6dd4b78c2a16066ece
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-08-26 17:41:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 //Button Counter Program
 
 var button = document.getElementById("counterbutton");
+
+var counter = 0;
+
+button.onclick = function(){
+    //Render counter in span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+};
