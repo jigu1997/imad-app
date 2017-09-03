745f9fccc5dbfc52ecf8a5ced715a1fcf668c252
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-09-03 22:44:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,10 +153,22 @@ app.get('/submit-name', function(req, res){
  
 });
 
-app.get('/:articlesparameter', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   
-  var articlename = req.params.articlesparameter;
-  res.send(createtemplate(articles[articlename]));
+  var articlename = req.params.articleName;
+  pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          if(result.rows.length===0){
+              res.status(404).send('Article not found');
+          }else{
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+  });
+  res.send(createtemplate(articleData));
 });
 
 var counter = 0;
