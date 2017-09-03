c08b18dd06db76851914ffc043ad79df3fe35856
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-09-03 22:30:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
 var app = express();
 app.use(morgan('combined'));
+
+var config = {
+    user : 'jigu1997',
+    database : 'jigu1997',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+}
 
 var articles = {
     
@@ -113,6 +122,19 @@ function createtemplate(data){
     
     return htmltemplate;
 }
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  //make select request
+  pool.query('SELECT * FROM test',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  });
+  //return response with result
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
