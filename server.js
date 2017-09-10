918efbd255a7e7b53bb3a17daa3bce8a5980e602
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-09-10 11:39:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/check-login',function(req,res){
 
 app.get('/logout',function(req,res){
    delete req.session.auth;
-   alert("Successfully logged out");
+   res.send("Logged out");
 });
 
 var pool = new Pool(config);
