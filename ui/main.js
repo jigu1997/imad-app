4de7d746eb738408f47c5120bb35e2d5d377d1e8
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-09-10 10:58:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,6 @@
-//Button Counter Program
+//Submit Username,Password
 
-var button = document.getElementById("counterbutton");
-
-button.onclick = function(){
-    
-    //Create Request
-    var request = new XMLHttpRequest();
-    
-    //Capture Response and Render
-    request.onreadystatechange = function(){
-        
-        if(request.readyState == XMLHttpRequest.DONE){
-            
-            if(request.status == 200){
-                var counter = request.responseText;
-                var span = document.getElementById("count");
-                span.innerHTML = counter.toString();
-                
-            }
-            
-        }
-        
-    };
-    
-    //Make Request
-    request.open("GET","http://jigu1997.imad.hasura-app.io/counter",true);
-    request.send(null);
-};
-
-//Submit Name
-
-var submit = document.getElementById("Submit");
+var submit = document.getElementById("submitbtn");
 
 submit.onclick = function(){
     
@@ -43,27 +13,25 @@ submit.onclick = function(){
      if(request.readyState == XMLHttpRequest.DONE){
          
          if(request.status == 200){
-             
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-            for(var i=0;i<names.length;i++){
-            list = list +"<li>"+names[i]+"</li>";
-            }
-            var ul = document.getElementById("namelist");
-            ul.innerHTML = list;
-
-             
+             alert("Welcome User");       
          }
+         else if(request.status==403){
+             alert("incorrect Credentials");
+         }
+         else if(request.status==500){
+             alert("Internal Server Error");
+         }
+         
          
      } 
         
     };
   
-    var nameinput = document.getElementById("name");
-    var namevalue = nameinput.value;
-    request.open("GET","http://jigu1997.imad.hasura-app.io/submit-name?name="+namevalue,true);
-    request.send(null);
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    request.open("POST","http://jigu1997.imad.hasura-app.io/login",true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
     
 };
 
