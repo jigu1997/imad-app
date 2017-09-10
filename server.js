81ceeb95c047e50ab587090e716af1b69b8cf8f5
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-09-10 10:34:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/login',function(req, res){
               res.status(403).send("Username / Password is Invalid");
           }
           else{
-              var dbString = res.rows[0].password;
+              var dbString = result.rows[0].password;
               var salt = dbString.split('%')[2];
               var hashedPassword = hash(password,salt);
               if(hashedPassword==dbString){
