c5e863bca6c0e71612aac06d2cceb40c8a0a0612
jigu1997 <30730119+jigu1997@users.noreply.github.com>
2017-10-01 09:34:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ submit.onclick = function(){
   
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    request.open("POST","http://jigu1997.imad.hasura-app.io/login",true);
+    request.open("POST","http://jigu1997.imad.hasura-app.io/create-user",true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username,password: password}));
     
